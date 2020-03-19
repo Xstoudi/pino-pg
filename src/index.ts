89013e832d447217594cd9636c235aebbbea50ad
Xavier Stouder <xavier@stouder.io>
2020-03-19 14:03:13 +0100

    feat: support node v10

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,47 @@
 import { Client } from 'pg'
 import split from 'split2'
-import { pipeline } from 'stream'
+import { pipeline, TransformCallback, Transform } from 'stream'
 
 import args from './args'
 
+class PgTransport extends Transform {
+  private client: Client;
+  private table: string;
+  private column: string;
+
+  constructor(table: string, column: string, client: Client) {
+    super()
+    this.table = table
+    this.column = column
+    this.client = client
+  }
+
+  _transform(chunk: any, encoding: string, callback: TransformCallback) {
+    const content = chunk.toString('utf-8')
+    this.client.query(`INSERT INTO ${this.table}(${this.column}) VALUES($1)`, [JSON.parse(content)])
+      .then(
+        () => {
+          callback(null, chunk)
+        },
+        err => callback(err, null)
+      )
+  }
+}
+
 function transporter(table: string, column: string, client: Client) {
-  return async function* (source: AsyncIterable<string>) {
+  const pgTransport = new PgTransport(table, column, client)
+  pgTransport.on('end', () => {
+    client.end()
+  })
+  return pgTransport
+
+  /*return async function* (source: AsyncIterable<string>) {
     for await (const line of source) {
       await client.query(`INSERT INTO ${table}(${column}) VALUES($1)`, [JSON.parse(line)])
       yield line
     }
-  }
+  }*/
+  
 }
 
 function main() {
@@ -19,11 +50,11 @@ function main() {
     if(connectErr !== null) {
       return console.error('Failed to connect to PostgreSQL server.', connectErr)
     }
+
     pipeline(process.stdin, split(), transporter(args.table, args.column, client) as any, process.stdout, err => {
-      if(err !== null){
+      if(err != null){
         console.error(err)
       }
-      client.end()
     })
   })
 }
